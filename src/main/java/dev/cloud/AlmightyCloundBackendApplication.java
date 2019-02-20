1be4c7fcb6475557b7c7a7af2f1c9e1dc607b7a2
Thomas Guyader <thomas.guyader@recif.nc>
2019-02-21 09:02:53 +1100

    import initial

---
+++ src/main/java/dev/cloud/AlmightyCloundBackendApplication.java
@@ -0,0 +1,13 @@
+package dev.cloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlmightyCloundBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlmightyCloundBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/cloud/AlmightyCloundBackendApplicationTests.java
@@ -0,0 +1,16 @@
+package dev.cloud;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class AlmightyCloundBackendApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
